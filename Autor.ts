3726f0feb64c1eb8fc05d295c8f596fefbe19710
rafaelOchoaMello <ochoagames2@gmail.com>
2021-04-08 09:45:44 -0300

    :zap: Project Refatoration
    Atualizado e testado implementação geral de alguns métodos das classes do projeto.

--- Autor.ts
+++ Autor.ts
@@ -1,29 +1,50 @@
+//Modificado por: Rafael Ochôa Mello - 06/04/21
+/* (Ajustes):
+
+- Alterada a imp. do constructor
+- Alteradas as assinaturas dos Gets'n Sets
+    dos atributos nome e data de nasc.
+- Criado método equals
+*/
+
 export default class Autor{
     private _nome: string
-    private _dataDeNascimento: Date
+    private _dataDeNascimento: Date;
 
-    constructor(nome: string, dataDeNascimento?: Date){
+    //Define como padrão uma data inválida caso nenhuma seja recebida
+    constructor(nome: string, dataDeNascimento: Date = new Date("0000-00-00")){
         this._nome = nome
         this._dataDeNascimento = dataDeNascimento
-
     }
 
-    public get nome() : string {
-    return this._nome
+    public get getNome() : string {
+        return this._nome;
     }
 
-    public set nome(nome: string){
-        this._nome = nome
+    public set setNome(nome: string){
+        this._nome = nome;
     }
 
-    public get dataDeNascimento() : Date {
-        return this._dataDeNascimento
+    public get getDataDeNascimento() : Date {
+        return this._dataDeNascimento;
     }
     
-    public set dataDeNascimento(dataDeNascimento: Date) {
-        this._dataDeNascimento = dataDeNascimento
+    public set setDataDeNascimento(dataDeNascimento: Date) {
+        this._dataDeNascimento = dataDeNascimento;
+    }
+
+    //Implementação do Equals para ajudar na remolção - Rafael
+    public equals(obj: object): boolean{
+        const capitulo = obj as Autor;
+
+        if( this.constructor !== obj.constructor )
+            return false;
+        else if(this._nome == undefined &&  this._dataDeNascimento == undefined)
+            return false;
+        else if((this._nome !== capitulo.getNome) && ( this._dataDeNascimento !== capitulo.getDataDeNascimento) )
+            return false;
+        else
+            return true;        
     }
 
 }
-
-
--- Livro.ts
+++ Livro.ts
@@ -1,90 +1,107 @@
+//Modificado por: Rafael Ochôa Mello - 06/04/21
+/* (Ajustes):
+
+- Alterada a imp. do constructor
+- Alteradas as assinaturas dos Gets'n Sets
+    dos atributos nome e data de nasc.
+- Re-implementados os métodos de adição e remoção de cap. e autor
+*/
+
 import Autor from "./Autor"
 import Capitulo from "./Capitulo"
 
-export class Livro{
-    private _titulo: string
-    private _ISBN: string
-    private _autores: Autor[]
-    private _capitulos: Capitulo[]
-    private _numeroDeCapitulos: number
+export default class Livro{
+    private _titulo: string;
+    private _ISBN: string;
+    private _autores: Autor[] = [];
+    private _capitulos: Capitulo[] = [];
+    private _numeroDeCapitulos: number = 0;
 
     constructor(titulo: string, ISBN: string){
-        this._ISBN = ISBN
-        this._titulo = titulo
+        this._ISBN = ISBN;
+        this._titulo = titulo;
     }
 
     
-    public get capitulos() : Capitulo[] {
-        return this._capitulos
+    public get getCapitulos() : Capitulo[] {
+        return this._capitulos;
     }
     
-    public set capitulos(capitulos: Capitulo[]){
-        this._capitulos = capitulos
-    }
-
-    
-    public get autores() : Autor[] {
-        return this.autores
-    }
-    
-    public set autores(autores: Autor[]){
-        this._autores = autores
+    public set setCapitulos(capitulos: Capitulo[]){
+        this._capitulos = capitulos;
     }
 
     
-    public get ISBN() : string {
-        return this._ISBN
+    public get getAutores() : Autor[] {
+        return this._autores;
+    }
+    
+    public set setAutores(autores: Autor[]){
+        this._autores = autores;
     }
 
     
-    public set ISBN(ISBN : string) {
+    public get getISBN() : string {
+        return this._ISBN;
+    }
+
+    
+    public set setISBN(ISBN : string) {
         this._ISBN = ISBN;
     }
 
+    //Métodos de Capítulos
     adicionarCapitulo(titulo: string, texto: string): number {
-        let tamanho1 = this._capitulos.length
-        this._capitulos.push(new Capitulo(titulo, texto))
-        let tamanho2 = this._capitulos.length
-        if (tamanho1 < tamanho2){
-            return tamanho1
-        } else {
-            return -1
-        }
+        const nCapitulos = this._capitulos.length;
 
+        this._capitulos.push(new Capitulo(titulo, texto))
+        if( nCapitulos >= this._capitulos.length)
+            return -1;
+        
+        this._numeroDeCapitulos++;
+        return nCapitulos;
     }
+
     removerCapitulo(capituloProcurado: Capitulo): number {
-        let i = 0
-        for (i=0;i<=this._capitulos.length;i++){
+        let i = 0;
+        
+        for (i=0; i <= this._capitulos.length; i++){
             if (this._capitulos[i].equals(capituloProcurado)){
                 this._capitulos.splice(i, 1)
-                return i
+                break;
             }
-        } if (i > this._capitulos.length - 1){
-            return -1
         }
+        if (i > this._capitulos.length - 1)
+            return -1;
+        
+        this._numeroDeCapitulos--;
+        return i;
     }
+
+    //Métodos de Autor
     adicionarAutor(nome: string, dataDeNascimento: Date): number {
-        let tamanho1 = this._autores.length
-        this._autores.push(new Autor(nome, dataDeNascimento))
-        let tamanho2 = this._autores.length
-        if (tamanho1 < tamanho2){
-            return tamanho1
-        } else {
-            return -1
-        }
+        const nAutores = this._autores.length;
 
+        this._autores.push(new Autor(nome, dataDeNascimento));
+        if( nAutores >= this._autores.length )
+            return -1;
+
+        return nAutores;
     }
-    removerAutor(autorProcurado: Autor): number{
-        let i = 0
-        for (i=0;i<=this._autores.length;i++){
-            if (this._autores[i].nome == autorProcurado.nome && this._autores[i].dataDeNascimento == autorProcurado.dataDeNascimento){
-                this._autores.splice(i, 1)
-                return i
-            }
-        } if (i > this._autores.length - 1){
-            return -1
-        }
-    }
+
     
-
+    removerAutor(autorProcurado: Autor): number{
+        let i = 0;
+        
+        for (i=0; i <= this._autores.length; i++){
+            if (this._autores[i].equals(autorProcurado)){
+                this._autores.splice(i, 1)
+                break;
+            }
+        }
+        if (i > this._autores.length - 1)
+            return -1;
+        
+        return i;
+    }
 }
